383ffe876f52c403d9120023de9b1151c79fbc0d
Himanshu <hs913271@gmail.com>
2022-03-03 10:50:46 +0530

    initial

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import axios from 'axios';
+import aws256 from 'aes256'
 
-export default function App() {
+const App = () => {
+  // barcodeRecognised = ({barcodes}) => {
+  //   barcodes.forEach(barcode => console.warn(barcode.data));
+  // }
+  const [isread, setIsread] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <RNCamera
+      ref={ref => {
+        this.camera = ref;
+      }}
+      style={{
+        flex: 1,
+        width: '100%'
+      }}
+      onBarCodeRead={(scan) => {
+        if (!isread) {
+          setIsread(true);
+          const array = scan.data.split(":");
+          var username = array[0];
+          var token = array[1];
+          console.log(scan.data);
+          axios.post( "/verifyToken", {
+            username: username,
+            key: token
+          })
+            .then(res => {
+              if (res.data === true) {
+                Alert.alert(
+                  "Success",
+                  "Student is Authenticated to enter the Campus", [
+                  {
+                    text: "OK",
+                    onPress: () => setIsread(false)
+                  }
+                ]
+                )
+              }
+              else {
+                Alert.alert(
+                  "Invalid QR Code",
+                  "This QR Code is Invalid", [
+                  {
+                    text: "OK",
+                    onPress: () => setIsread(false)
+                  }
+                ]
+                )
+              }
+            })
+            .catch(err => console.log(err));
+        }
+      }}
+    ></RNCamera>
+
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
